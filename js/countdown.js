5e03a22d5d69f224115922134726cc68d62d8da8
0zhangjun <36331915+0zhangjun@users.noreply.github.com>
2018-06-01 19:22:51 +0800

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 1, 2018 18:30:00").getTime();
+var countDownDate = new Date("Jun 1, 2018 23:00:00").getTime();
 var music = false;
 // Update the count down every 1 second
 var x = setInterval(function() {
